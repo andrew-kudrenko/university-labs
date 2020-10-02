92e30ec3416ef11f9ad921e80dda144adcc89ad9
Andrew <48319785+isAloneWanderer@users.noreply.github.com>
2020-10-02 15:58:01 +0300

    fixed

--- array_sort.py
+++ array_sort.py
@@ -1,16 +1,29 @@
 from typing import List
 
 
-def swap(collection: List, from_idx: int, to_idx: int):
-  buffer = collection[from_idx]
-  collection[from_idx] = collection[to_idx]
-  collection[to_idx] = buffer
-
-
 def sort_by_bubble(collection: List):
   length = len(collection)
 
   for i in range(length):
     for j in range(i, length):
       if collection[i] > collection[j]:
-        swap(collection, i, j)
+        collection[i], collection[j] = collection[j], collection[i]
+
+
+a = [9, 4, 2, 102, 32, 11, 9, 2, 0, 9]
+
+i = 0
+
+while i < len(a):
+  k = i
+  while k < len(a):
+    if a[i] > a[k]:
+      a[i], a[k] = a[k], a[i]
+    k += 1
+
+  i += 1
+
+
+#sort_by_bubble(a)
+
+print(a)
