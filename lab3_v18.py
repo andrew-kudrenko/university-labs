b2391d70b688c316209bdd408e2757453f422882
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2020-11-11 14:24:11 +0300

    lab 3

---
+++ lab3_v18.py
@@ -0,0 +1,23 @@
+array = list(map(int, input('Enter an array: ').split()))
+sub_array = list(map(int, input('Enter an sub array: ').split()))
+
+intersected = False
+is_subset = False
+
+for idx, item in enumerate(array):
+    if not sub_array:
+        is_subset = True
+        break
+
+    if sub_array[0] == item:
+        sliced = array[idx:idx+len(sub_array)]
+        print(sliced, sub_array)
+        for s_idx, s_item in enumerate(sliced):
+            if s_item != sub_array[s_idx]:
+                break
+
+            is_subset = True
+
+        break
+
+print(f'{"Является" if is_subset else "Не является"} подмассивом')
