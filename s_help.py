92dd845e3865597206fa0129a18a95a7d5214ef6
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2020-11-18 00:22:16 +0300

    help

---
+++ s_help.py
@@ -0,0 +1,27 @@
+def fill_matrix(matrix, rows, cols):
+    print('Enter a matrix')
+
+    for row in range(rows):
+        for col in range(cols):
+            matrix[row][col] = int(input('> '))
+
+
+rows = int(input('Rows: '))
+cols = int(input('Cols: '))
+
+a = [[0] * cols for row in range(rows)]
+
+fill_matrix(a, rows, cols)
+
+max_sum = 0
+
+for col in range(cols):
+    sum_of_col = 0
+
+    for row in range(rows):
+        sum_of_col += a[row][col]
+
+    if sum_of_col > max_sum:
+        max_sum = sum_of_col
+
+print(max_sum)
