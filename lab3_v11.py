acb153a65a802b0975c1b426d84fbe3ad924cb29
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2020-11-17 16:02:31 +0300

    removed unused

--- lab3_v11.py
+++ lab3_v11.py
@@ -15,31 +15,26 @@ for i, symbol in enumerate(input_data):
 
     prev_symbol = symbol
 
-if letters_counter == 0:
-    print('Не найдено ни одного слова')
-elif need_length < len(input_data):
-    print('Требуемая длина не должна быть меньше исходной')
-elif words_counter <= 1:
-    print(input_data)
-else:
-    whitespaces_per_word: int = need_length // (words_counter - 1)
-    whitespaces_rest: int = need_length % (words_counter - 1)
 
-    concatenated: str = ''
+whitespaces_per_word: int = (need_length - letters_counter) // (words_counter - 1)
+whitespaces_rest: int = (need_length - letters_counter) % (words_counter - 1)
 
-    prev_symbol = ''
-    replaced_whitespace_index: int = 0
+concatenated: str = ''
 
-    for i, symbol in enumerate(input_data):
-        if symbol != ' ':
-            concatenated += symbol
-        elif replaced_whitespace_index == 0 and words_counter == 1:
-            concatenated += ' ' * whitespaces_per_word + ' ' * whitespaces_rest
-        elif replaced_whitespace_index != i:
-            replaced_whitespace_index = i
-            concatenated += ' ' * whitespaces_per_word
-            replaced_whitespace_index = i
+prev_symbol = ''
+current_word = 0
 
-        prev_symbol = symbol
+for i, symbol in enumerate(input_data):
+    if symbol == ' ' and prev_symbol != ' ' or symbol != ' ' and i == len(input_data) - 1:
+        current_word += 1
 
-    print(concatenated)
+    if symbol != ' ':
+        concatenated += symbol
+    elif current_word == words_counter - 1:
+        concatenated += ' ' * (whitespaces_per_word + whitespaces_rest)
+    else:
+        concatenated += ' ' * whitespaces_per_word
+
+    prev_symbol = symbol
+
+print(concatenated)
