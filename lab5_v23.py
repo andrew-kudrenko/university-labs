c0b20eeefd4a5d47369b1c388d7789d8e9a8f9b5
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2020-12-14 19:00:51 +0300

    commerce

---
+++ lab5_v23.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def take_above(matrix, side=False):
+    taken = []
+
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            if (not side and (x > y)) or (side and (x < len(matrix) - y - 1)):
+                taken.append(matrix[y][x])
+
+    return taken
+
+
+def add_above_main(matrix):
+    sum = 0
+
+    for item in take_above(matrix):
+        sum += item
+
+    return sum
+
+
+def mult_above_side(matrix):
+    product = 1
+
+    for item in take_above(matrix, True):
+        product *= item
+
+    return product
+
+
+print('Size of matrix')
+
+rows = int(input('Rows = '))
+cols = int(input('Cols = '))
+
+matrix = np.random.randint(1, 10, (rows, cols))
+
+print('Initial matrix\n', matrix)
+print(f'Summary equals {add_above_main(matrix)}\nProduct equals {mult_above_side(matrix)}')
