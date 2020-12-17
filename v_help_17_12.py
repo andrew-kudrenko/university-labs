7333413f0ad2324a19e6c4c4bd6f902ebbcf0cf8
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2020-12-17 15:33:53 +0300

    labs

--- v_help_17_12.py
+++ v_help_17_12.py
@@ -1,12 +1,16 @@
+def check_for_intersections(a, b):
+    vowels = 'ёуеыаоэяию'
+    intersections = ''
+
+    for letter in vowels:
+        lowed = letter.lower()
+        if (lowed in a) and (lowed in b):
+            intersections += letter
+
+    return intersections
+
+
 user_string = input('Введите строку: ')
 user_word = input('Введите слово: ')
 
-vowels = 'ёуеыаоэяию'
-intersections = ''
-
-for letter in vowels:
-    lowed = letter.lower()
-    if (lowed in user_string) and (lowed in user_word):
-        intersections += letter
-
-print(intersections)
+print(check_for_intersections(user_string, user_word))
